d53675049f1ce174f7957f724981fffa4fae9de3
Misenpai <misenpai200231@gmail.com>
2024-02-12 19:57:49 +0530

    Second

--- lib/pages/research_page.dart
+++ lib/pages/research_page.dart
@@ -1,73 +1,88 @@
 import 'package:flutter/material.dart';
 
-class ResearchPaperSearch extends StatefulWidget {
-  @override
-  _ResearchPaperSearchState createState() => _ResearchPaperSearchState();
-}
-
-class _ResearchPaperSearchState extends State<ResearchPaperSearch> {
-  TextEditingController _searchController = TextEditingController();
-  List<String> _searchResults = [];
-
-  void _searchPapers(String query) {
-    // Simulate search results
-    List<String> results = [
-      "Paper 1",
-      "Paper 2",
-      "Paper 3",
-      "Paper 4",
-      "Paper 5",
-    ];
-
-    setState(() {
-      _searchResults = results;
-    });
-  }
+class ResearchPaperSearch extends StatelessWidget {
+  const ResearchPaperSearch({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Medical Research Papers'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                labelText: 'Search',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    _searchPapers(_searchController.text);
-                  },
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Researcher App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(16.0),
+                margin: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Title 1',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          SizedBox(height: 5.0),
+                          Text(
+                            'This is some text below the title 1.',
+                            style: TextStyle(fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                margin: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Title 2',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          SizedBox(height: 5.0),
+                          Text(
+                            'This is some text below the title 2.',
+                            style: TextStyle(fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _searchResults.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  padding: EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Text(
-                    _searchResults[index],
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
